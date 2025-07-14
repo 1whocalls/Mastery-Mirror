032f28330ea5eae0a93059bd9e1e44ac6b1b2673
1whocalls <1whocalls@protonmail.com>
2025-07-14 15:06:58 +0200

    More error handling fixes

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ router.get('/account/:region/:gamename/:tagline', async (request, env) => {
     try {
         const fetchResponse = await fetcher().get(`https://${request.params.region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${request.params.gamename}/${request.params.tagline}?api_key=${env.API_KEY}`);
         return json(fetchResponse);
-    } catch {
-        return error(404, "Not found.");
+    } catch (err) {
+        return error(err.status.status_code, err.status.message);
     }
 });
 
@@ -22,8 +22,8 @@ router.get('/iconcode/:region/:puuid', async (request, env) => {
     try {
         const fetchResponse = await fetcher().get(`https://${request.params.region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${request.params.puuid}?api_key=${env.API_KEY}`);
         return json(fetchResponse);
-    } catch {
-        return error(404, "Not found.");
+    } catch (err) {
+        return error(err);
     }
 });
 
